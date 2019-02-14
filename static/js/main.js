f8554cffdfb5617e9f1296c7509a45cd1c0f49da
Chad Spencer <chad@chadspencer.com>
2019-02-13 19:59:56 -0600

    brought back nav click and hover on desktop

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
 
-  // function nav() {
-  //   $('nav span').each(function( index ) {
-  //     $(this).click(function() {
-  //       $('body').addClass('scrolling');
-  //       $('html, body').animate({
-  //         scrollTop: 100 * (index + 1)
-  //       }, 500, function() {
-  //         $('body').removeClass('scrolling');
-  //       });
-  //     });
-  //   });
-  // }
-  // nav();
+  if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    function nav() {
+      $('nav span').each(function( index ) {
+        $(this).click(function() {
+          $('body').addClass('scrolling');
+          $('html, body').animate({
+            scrollTop: 100 * (index + 1)
+          }, 500, function() {
+            $('body').removeClass('scrolling');
+          });
+        });
+      });
+    }
+    nav();
+  }
   
   function scroll() {
     var scrollPosition = $(document).scrollTop();
