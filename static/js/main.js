11a9c574236d11a68f92d793cf2c36880bf869ec
Chad Spencer <chad@chadspencer.com>
2019-02-13 19:54:26 -0600

    temporarily disabled nav clicks

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,27 @@
 $(document).ready(function() {
 
+  // function nav() {
+  //   $('nav span').each(function( index ) {
+  //     $(this).click(function() {
+  //       $('body').addClass('scrolling');
+  //       $('html, body').animate({
+  //         scrollTop: 100 * (index + 1)
+  //       }, 500, function() {
+  //         $('body').removeClass('scrolling');
+  //       });
+  //     });
+  //   });
+  // }
+  // nav();
+  
   function scroll() {
     var scrollPosition = $(document).scrollTop();
     var sectionHeight = $('main').outerHeight();
     $('main').scrollTop(sectionHeight * (scrollPosition / 100));
   }
-
-  function nav() {
-    $('nav span').each(function( index ) {
-      $(this).click(function() {
-        $('body').addClass('scrolling');
-        $('html, body').animate({
-          scrollTop: 100 * (index + 1)
-        }, 500, function() {
-          $('body').removeClass('scrolling');
-        });
-      });
-    });
-  }
-
-  nav();
   scroll();
 
-  $(window).scroll(function() {
+  $(window).on('scroll', function(e) {
     scroll();
   });
   
