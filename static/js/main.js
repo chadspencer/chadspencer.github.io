811e22a5bdf58865ac82c422d45a6644e9a20aa3
Chad Spencer <chad@chadspencer.com>
2019-02-13 20:57:45 -0600

    fixed nav mobile issues

--- static/js/main.js
+++ static/js/main.js
@@ -1,30 +1,67 @@
 $(document).ready(function() {
+  function nav() {
+    $("nav span").each(function(index) {
+      $(this).click(function() {
+        var scrollOffset = 100 * (index + 1) + 2;
+        var currentScroll = $("html, body").scrollTop();
+        $("body").addClass("scrolling");
+        if (navigator.userAgent.match(/iPad|iPhone|iPod|Android|Windows Phone/i)) {
+          function customScrollTo(to, duration) {
+            var start = window.pageYOffset,
+              change = to - start,
+              currentTime = 0,
+              increment = 20;
 
-  if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    function nav() {
-      $('nav span').each(function( index ) {
-        $(this).click(function() {
-          $('body').addClass('scrolling');
-          $('html, body').animate({
-            scrollTop: 100 * (index + 1)
-          }, 500, function() {
-            $('body').removeClass('scrolling');
-          });
-        });
+            var animateScroll = function() {
+              currentTime += increment;
+              var val = Math.easeInOutQuad(
+                currentTime,
+                start,
+                change,
+                duration
+              );
+              window.scrollTo(0, val);
+
+              if (currentTime < duration) {
+                setTimeout(animateScroll, increment);
+              }
+            };
+            animateScroll();
+          }
+
+          Math.easeInOutQuad = function(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return (c / 2) * t * t + b;
+            t--;
+            return (-c / 2) * (t * (t - 2) - 1) + b;
+          };
+
+          customScrollTo(scrollOffset, 250);
+        } else {
+          $("html, body").animate(
+            {
+              scrollTop: scrollOffset
+            },
+            500,
+            function() {
+              $("body").removeClass("scrolling");
+            }
+          );
+        }
       });
-    }
-    nav();
+    });
   }
-  
+  nav();
+
   function scroll() {
     var scrollPosition = $(document).scrollTop();
-    var sectionHeight = $('main').outerHeight();
-    $('main').scrollTop(sectionHeight * (scrollPosition / 100));
+    var sectionHeight = $("main").outerHeight();
+    $("main").scrollTop(sectionHeight * (scrollPosition / 100));
   }
   scroll();
 
-  $(window).on('scroll', function(e) {
+  $(window).scroll(function() {
     scroll();
   });
-  
+
 });
