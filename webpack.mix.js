aa276981f47e4864f25a9494f1c1146c4132978f
Chad Spencer <chad@chadspencer.com>
2019-03-04 08:51:54 -0600

    fixing build

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,19 +3,19 @@ const mix = require('laravel-mix'), fs = require('fs-extra')
 mix.version()
     .disableNotifications()
     .setPublicPath('_site')
-    .sass('_assets/scss/main.scss', '_site/static/css').options({
+    .sass('static/scss/main.scss', '_site/static/css').options({
         processCssUrls: false
      })
-    .js('_assets/js/main.js', '_site/static/js')
+    .js('static/js/main.js', '_site/static/js')
     .extract([
         'jquery',
     ])
     .autoload({
         jquery: ['$', 'window.jQuery', 'jQuery']
     })
-    .copy('_assets/js/html5shiv.js', '_site/static/js')
-    .copyDirectory('_assets/fonts', '_site/static/fonts')
-    .copyDirectory('_assets/img', '_site/static/img')
+    .copy('static/js/html5shiv.js', '_site/static/js')
+    .copyDirectory('static/fonts', '_site/static/fonts')
+    .copyDirectory('static/img', '_site/static/img')
     .sourceMaps()
     .version()
       .then(() => fs.copy('_site/mix-manifest.json', '_data/mix-manifest.json'))
