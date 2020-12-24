0918264de1a20921af039f0ecb7bea4d5fef88aa
Chad Spencer <cspencer@paylocity.com>
2020-12-24 11:38:28 -0600

    updates

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
     $("nav span").each(function(index) {
       $(this).click(function() {
         var scrollOffset = 100 * (index + 1);
-        // $("body").addClass("scrolling");
         if (navigator.userAgent.match(/iPad|iPhone|iPod|Android|Windows Phone/i)) {
+          $("body").addClass("scrolling");
           function customScrollTo(to, duration) {
             var start = window.pageYOffset,
               change = to - start,
@@ -30,9 +30,9 @@ $(document).ready(function() {
             animateScroll();
           }
 
-          // function removeBodyClass() {
-          //   $("body").removeClass("scrolling")
-          // }
+          function removeBodyClass() {
+            $("body").removeClass("scrolling")
+          }
 
           Math.easeInOutQuad = function(t, b, c, d) {
             t /= d / 2;
@@ -42,7 +42,7 @@ $(document).ready(function() {
           };
 
           customScrollTo(scrollOffset, 250);
-          // setTimeout(removeBodyClass, 250);
+          setTimeout(removeBodyClass, 250);
         } else {
           $("html, body").animate(
             {
